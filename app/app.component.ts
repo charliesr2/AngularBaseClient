b8b6c5a3285f4907fd0cc4b920538b8adb78ad1c
Carlos <charliesr2@gmail.com>
2017-10-29 14:16:25 +0100

    Base Project for angular2 Hello World

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({    
+    selector: 'my-app',
+    template: '<h1>Hello World, Welcome to Angular2</h1>'
+})
+
+
+export class AppComponent {
+
+}
